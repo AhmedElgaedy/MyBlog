c0db9058e0d12a963dc39650fde771f293bb9f9a
AhmedElgeady <a.mostafa199964@gmail.com>
2023-10-24 13:58:13 +0300

    first commit

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,181 @@
+<?php
+
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use PDO;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+
+class CategoryController extends Controller
+{
+    public function index(){
+
+        try{
+
+            $categories = Category::orderBy('id','desc')->get();
+            if($categories){
+            return response()->json([
+                'success' => true,
+                'categories' => $categories
+            ]);
+            };
+            }
+            catch(Exception $e){
+
+               return response()->json([
+                    'success' => false,
+                    'message' => $e->getMessage()
+                ]);
+            
+          }
+    }
+    
+
+
+    public function store (Request $request){
+        
+        try{   
+
+            $validation = Validator::make($request->all(),[
+
+            'name'=> ['required','string','max:20','min:10','unique:categories'],
+
+            ]); 
+            if($validation->fails()){
+                return response()->json([
+                    'success'=>false,
+                    'message'=> $validation->errors(),
+                ]);
+            }else{
+                $result = Category::create([
+                    'name'=> $request->name,
+                ]);
+                if($result){
+                    return response()->json([
+                        'success'=>true,
+                        'message'=>'category added successfuly',
+                    ]);
+                }else{
+                    return response()->json([
+                        'success'=>false,
+                        'message'=>' a problem happened ',
+                    ]);
+                }
+            }
+        }catch(Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => $e ->getMessage(),
+            ]);
+        
+        }
+    }
+
+    public function edit($id){
+        try {
+
+            $categories = Category::findOrFail($id);
+            if ($categories) {
+                return response()->json(
+                    [
+                        'success' => true,
+                        'categories' => $categories
+                    ]
+                );
+            }
+        } catch (Exception $e) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'categories' => $e->getMessage()
+                ]
+            );
+        }
+    }
+
+
+    public function update(Request $request, $id){
+        try{
+            $categories = Category::findOrFail($id);
+        $validation= Validator::make($request->all(), [
+            'name'=> ['required','string','max:20','min:10','unique:categories'],
+        ]);
+
+        if($validation->fails()){
+            return response()->json([
+                'success'=> false,
+                'message'=> $validation->errors()->all(),
+            ]);
+        }else{
+            $categories->name = $request->name;
+            $result = $categories->save();
+            if($result){
+                return response()->json([
+                    'success'=> true,
+                    'message'=> 'category updated successfully'
+                ]);
+            }else{
+                return response()->json([
+                    'success'=> false,
+                    'message'=> 'a problem happened',
+                ]);
+            }
+            }
+        }catch (Exception $e) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'categories' => $e->getMessage()
+                ]
+            );
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $result = Category::findOrFail($id)->delete();
+            if ($result) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Category Delete Successfully'
+                ]);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'a problem happened',
+                ]);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public function search($search)
+    {
+        try {
+            $categories = Category::where('category_name', 'LIKE', '%' . $search . '%')->orderBy('id', 'desc')->get();
+            if ($categories) {
+                return response()->json([
+                    'success' => true,
+                    'categories' => $categories
+                ]);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+}
+
---
+++ database/migrations/2023_10_16_184840_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->string('Published');
+            $table->string('image');
+            $table->integer ('views');
+            $table->unsignedBigInteger('author_id');
+             $table->foreign('author_id')->references('id') ->on('authors')->onDelete('cascade');
+            // $table->foreign('author_id')->constraint('authors')->OnDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
